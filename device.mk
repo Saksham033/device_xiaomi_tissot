5bf25ecdd91c3850892b1b5de1f57a2d8de536b0
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-09-06 06:06:08 +0000

    Disable telephony-ext

--- device.mk
+++ device.mk
@@ -376,11 +376,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service
 
 # Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
+#PRODUCT_PACKAGES += \
+#    telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
